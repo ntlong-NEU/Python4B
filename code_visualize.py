532946343989001a4fda5c4a586d183fe6adc8d1
ntlong-NEU <ntlong@neu.edu.vn>
2026-06-12 21:18:30 +0700

    b5

--- code_visualize.py
+++ code_visualize.py
@@ -1,2 +1,8 @@
-for item in [1,3,4,2,5]:
-    print(item)
+text = "sd sd aa ab aa ac ab"
+fre_words = {}
+
+for w in text.split():
+    if w in fre_words:
+        fre_words[w] += 1
+    else:
+        fre_words[w] = 1
